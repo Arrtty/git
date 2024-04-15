b57a4da1a1dc7337c5d14ab55c692efccbd2445a
MOISKY <61997974+MOISKY@users.noreply.github.com>
2024-04-15 06:06:20 -0600

    Add files via upload

---
+++ suma.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("Estos son mis cambios en git para la prueba de verificacion y validacion");
+    for(int i =0;i < 10;i++){
+        printf(i);
+        print("Estos son cambios de git");
+    }
+    return 0;
+}
